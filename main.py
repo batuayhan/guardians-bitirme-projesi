d0dfe79c9a6661c235ad2f82e11611ada25bb5a8
Furkan Dolaşık <furkandol0640@gmail.com>
2022-03-06 18:42:02 +0300

    class yapisina donusturuldu. windowsta hata veren tema kaldırıldı.

--- main.py
+++ main.py
@@ -1,57 +1,55 @@
 import tkinter as tk
-from tkinter import *
 from tkinter import ttk
 
-window = tk.Tk()
-window.title("Guardians")
-window.geometry("200x100")
-window.configure(bg="#e8e8e8")
 
-s = ttk.Style(window)
-s.theme_use('aqua')
+class View(tk.Frame):
 
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        b = tk.Button(self, text="Open new window", command=self.new_window)
+        b.place(x=40,y=25,height=40)
 
-def openNewWindow():
-    newWindow = Toplevel(window)
-    newWindow.title("Guardians")
-    newWindow.geometry("510x420")
-    newWindow.configure(bg="#e8e8e8")
-    newWindow.columnconfigure(0, weight=1)
-    newWindow.columnconfigure(1, weight=1)
-    newWindow.columnconfigure(2, weight=3)
+    def new_window(self):
+        window = tk.Toplevel(self)
+        window.title("Guardians")
+        window.geometry("510x420")
+        window.configure(bg="#e8e8e8")
 
-    buttonKimlik = ttk.Button(newWindow, text="Kimlik Doğrulama")
-    buttonKimlik.grid(column=0, row=0, sticky=W, padx=5, pady=5)
+        buttonKimlik = ttk.Button(window, text="Kimlik Doğrulama")
+        buttonKimlik.place(x=25, y=25, height=50)
 
-    c = ttk.Checkbutton(newWindow)
-    c.grid(column=1, row=0,sticky=W)
+        c = ttk.Checkbutton(window)
+        c.place(x=175, y=40)
 
-    buttonKagit = ttk.Button(newWindow, text="Kağıt Kontrolü")
-    buttonKagit.grid(column=0, row=2, sticky=W, padx=5, pady=5)
+        buttonKagit = ttk.Button(window, text="Kağıt Kontrolü")
+        buttonKagit.place(x=25, y=100, height=50, width=139)
 
-    c2 = ttk.Checkbutton(newWindow)
-    c2.grid(column=1,row=2,sticky=W)
+        c2 = ttk.Checkbutton(window)
+        c2.place(x=175, y=110)
 
-    saatLabel = ttk.Label(newWindow, text="Saat")
-    saatLabel.grid(column=1,row=0,sticky=E,padx=160)
+        saatLabel = ttk.Label(window, text="Saat")
+        saatLabel.place(x=300, y=25)
 
-    saatEntry = ttk.Entry(newWindow)
-    saatEntry.insert(0,"Kalan Dakika: 45")
-    saatEntry.grid(column=1,row=1,sticky=E,padx=5)
+        saatEntry = ttk.Entry(window)
+        saatEntry.insert(0, "Kalan Dakika: 45")
+        saatEntry.config(state='disabled')
+        saatEntry.place(x=300, y=50)
 
-    kameraLabel = ttk.Label(newWindow, text="Öğrenci Kamerası")
-    kameraLabel.grid(column=1, row=2,sticky=E,padx=80,pady=5)
+        kameraLabel = ttk.Label(window, text="Öğrenci Kamerası")
+        kameraLabel.place(x=300, y=100)
 
-    kameraEntry = ttk.Entry(newWindow)
-    kameraEntry.grid(column=1, row=3, padx=5,pady=5,ipady=100,sticky=E)
+        kameraEntry = ttk.Entry(window)
+        kameraEntry.config(state='disabled')
+        kameraEntry.place(x=300, y=125, height=175)
 
-    submitButton = ttk.Button(newWindow, text="Submit")
-    submitButton.grid(column=1,row=6,padx=5,pady=30,sticky=E)
+        submitButton = ttk.Button(window, text="Submit")
+        submitButton.place(x=400, y=370, height=30)
 
-
-button = ttk.Button(window, text='Click to login with Google',
-
-                          command=openNewWindow)
-button.grid(column=0, row=2)
-
-window.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    view = View(root)
+    root.title("Guardians")
+    root.geometry("200x100")
+    root.configure(bg="#e8e8e8")
+    view.pack(side="top", fill="both", expand=True)
+    root.mainloop()
