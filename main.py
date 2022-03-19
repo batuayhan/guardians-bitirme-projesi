82e6a7f2876601ed3becb714f5b39efb4a78b87c
Batuhan Ayhan <batuhanayhan98@gmail.com>
2022-03-19 12:06:36 +0300

    main.py'ye thread yapısı eklendi. Kamera goruntuleri gui'ye yerlestirildi

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-
+import camera
+from PIL import Image, ImageTk
+from threading import Thread, Lock
 
 class View(tk.Frame):
 
@@ -9,44 +11,54 @@ class View(tk.Frame):
         b = tk.Button(self, text="Login with Google", command=self.new_window)
         self.configure(bg="#e8e8e8")
         b.place(x=40,y=25,height=40)
+        self.window = tk.Toplevel(self)
 
     def new_window(self):
-        window = tk.Toplevel(self)
-        window.title("Guardians")
-        window.geometry("510x420")
-        window.configure(bg="#e8e8e8")
+        self.window.title("Guardians")
+        self.window.geometry("510x420")
+        self.window.configure(bg="#e8e8e8")
 
-        buttonKimlik = ttk.Button(window, text="Kimlik Doğrulama")
+        buttonKimlik = ttk.Button(self.window, text="Kimlik Doğrulama")
         buttonKimlik.place(x=25, y=25, height=50)
 
-        c = ttk.Checkbutton(window)
+        c = ttk.Checkbutton(self.window)
         c.place(x=175, y=40)
 
-        buttonKagit = ttk.Button(window, text="Kağıt Kontrolü")
+        buttonKagit = ttk.Button(self.window, text="Kağıt Kontrolü")
         buttonKagit.place(x=25, y=100, height=50, width=139)
 
-        c2 = ttk.Checkbutton(window)
+        c2 = ttk.Checkbutton(self.window)
         c2.place(x=175, y=110)
 
-        saatLabel = ttk.Label(window, text="Saat")
+        saatLabel = ttk.Label(self.window, text="Saat")
         saatLabel.place(x=300, y=25)
 
-        saatEntry = ttk.Entry(window)
+        saatEntry = ttk.Entry(self.window)
         saatEntry.insert(0, "Kalan Dakika: 45")
         saatEntry.config(state='disabled')
         saatEntry.place(x=300, y=50)
 
-        kameraLabel = ttk.Label(window, text="Öğrenci Kamerası")
-        kameraLabel.place(x=300, y=100)
+        t2 = Thread(target=self.show_cam, args=(200,150))
+        t2.start()
 
-        kameraEntry = ttk.Entry(window)
-        kameraEntry.config(state='disabled')
-        kameraEntry.place(x=300, y=125, height=175)
-
-        submitButton = ttk.Button(window, text="Submit")
+        submitButton = ttk.Button(self.window, text="Submit")
         submitButton.place(x=400, y=370, height=30)
 
+    def show_cam(self,x,y):
+        while True:
+            imageframe = studentCam.image_frame(x,y)
+            imageframe = ImageTk.PhotoImage(image=imageframe)
+            kameraLabel = ttk.Label(self.window, image=imageframe)
+            kameraLabel.image = imageframe
+            kameraLabel.place(x=300,y=150)
+
+global studentCam
+def start_camera():
+        global studentCam
+        studentCam = camera.start_camera()
+
 if __name__ == "__main__":
+    start_camera()
     root = tk.Tk()
     view = View(root)
     root.title("Guardians")
