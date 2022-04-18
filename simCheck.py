a6f2e9cb2d0921e3052fc4378d00209245601b86
furkanorten <60614040+furkanorten@users.noreply.github.com>
2022-04-18 13:20:48 +0300

    hotfix simCheck

--- simCheck.py
+++ simCheck.py
@@ -5,16 +5,16 @@ def simCheck(img1, img2):
     image_one = cv2.imread(img1)
     image_two = cv2.imread(img2)
 
+    result = ""
     #Check if two images are equals
     if image_one.shape == image_two.shape:
-        print("The images have same size and channels")
         difference = cv2.subtract(image_one, image_two)
         b, g, r = cv2.split(difference)
 
         if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
-            print("The images are completely Equal")
+            result = "Good similarity"
         else:
-            print("The images are NOT equal")
+            result = "Bad similarity"
 
     #Check similarities between the 2 images
     sift = cv2.xfeatures2d.SIFT_create()
@@ -42,7 +42,6 @@ def simCheck(img1, img2):
     #print("Keypoints Image one: " + str(len(kp_1)))
     #print("Keypoints Image two: " + str(len(kp_2)))
     #print("Good Matches:", len(good_points))
-    result = ""
     if number_keypoints == 0:
         print("Match: 0/100")
         result = "Bad similarity"
